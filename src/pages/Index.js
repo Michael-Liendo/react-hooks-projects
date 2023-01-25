e21b7ce2c41321fba66d2f6a9cc54552bcfd765f
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-01-24 22:39:10 -0400

    explain this project

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Index() {
-  return <h1>Index</h1>;
+  return (
+    <h1>
+      This is a project to learn and create simple systems to learn react hooks
+    </h1>
+  );
 }
